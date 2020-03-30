294e8a98eef112faa3a343ddd8ea4fe6c74e8842
Fathan azka <62229232+fathanazka354@users.noreply.github.com>
2020-03-30 20:51:49 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace TheCoffeMechine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("COFFE MAKER");
+
+            CoffePowder coffe = new CoffePowder(100);
+            WaterGalon water = new WaterGalon(1000);
+            Milk milk = new Milk(300);
+            CoffeMechine yummyCoffe = new CoffeMechine(coffe, water, milk);
+
+            Console.WriteLine("check " + yummyCoffe.checkAvailability());
+            string result = yummyCoffe.makeCappucino();
+            Console.WriteLine("result " + result);
+
+            Console.WriteLine("check" + yummyCoffe.checkAvailability());
+            result = yummyCoffe.makeEsspresso();
+            Console.WriteLine("result" + result);
+
+            Console.WriteLine("check" + yummyCoffe.checkAvailability());
+            result = yummyCoffe.makeEsspresso();
+            Console.WriteLine("result" + result);
+
+            Console.WriteLine("check" + yummyCoffe.checkAvailability());
+            result = yummyCoffe.makeCappucino();
+            Console.WriteLine("result" + result);
+
+            Console.WriteLine("check" + yummyCoffe.checkAvailability());
+            result = yummyCoffe.makeEsspresso();
+            Console.WriteLine("result" + result);
+
+            Console.WriteLine("check" + yummyCoffe.checkAvailability());
+
+        }
+
+    }
+}
